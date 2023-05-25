1ac2515dd45e6620f6a4637b8782a1b690c57de4
Frank Kong <frankkongnj@gmail.com>
2023-05-25 21:53:44 +0800

    add for google bigquery

--- API-request-example.js
+++ API-request-example.js
@@ -3,22 +3,78 @@
 //   url: `https://restapi.amap.com/v3/weather/weatherInfo?city=110000&key=<YOUR_API_KEY>`,
 // })
 
-const tempRequest = Functions.makeHttpRequest({
-  url: `https://dataservice.accuweather.com/currentconditions/v1/300597?apikey=<YOUR_API_KEY>`,
-})
+const crypto = require("crypto")
+const querystring = require("querystring")
 
-const [tempResponse] = await Promise.all([tempRequest])
+const getOauthToken = async (iss, key) => {
+  // Replace \n character with actual newline character
+  const privateKey = key.replace(/\\n/g, "\n")
+  // const privateKey = key
 
-const temps = []
+  console.log("privatekey: " + privateKey)
 
-if (!tempResponse.error) {
-  // temps.push(tempResponse.data.lives[0].temperature)
-  temps.push(tempResponse.data[0].Temperature.Metric.Value)
-} else {
-  console.log("temp response error")
+  const jwtBase64Headers = Buffer.from('{"alg":"RS256","typ":"JWT"}').toString("base64")
+
+  const currentTimeInSeconds = Math.round(Date.now() / 1000)
+
+  const jwtClaimSetObj = {
+    iss: iss,
+    scope: "https://www.googleapis.com/auth/cloud-platform.read-only",
+    aud: "https://oauth2.googleapis.com/token",
+    exp: currentTimeInSeconds + 3500,
+    iat: currentTimeInSeconds,
+  }
+
+  const jwtBase64ClaimSet = Buffer.from(JSON.stringify(jwtClaimSetObj)).toString("base64")
+
+  const stringToSign = `${jwtBase64Headers}.${jwtBase64ClaimSet}`
+  const jwtBase64Signature = crypto.sign("RSA-SHA256", stringToSign, privateKey).toString("base64")
+  const jwtRequest = {
+    grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+    assertion: `${jwtBase64Headers}.${jwtBase64ClaimSet}.${jwtBase64Signature}`,
+  }
+
+  const jwtRequestString = querystring.stringify(jwtRequest)
+
+  console.log("jwt: " + jwtRequestString)
+
+  const tokenResponse = await Functions.makeHttpRequest({
+    url: "https://oauth2.googleapis.com/token",
+    method: "post",
+    data: jwtRequestString,
+  })
+
+  console.log("tokenResponse: " + tokenResponse)
+
+  return tokenResponse.data.access_token
 }
 
-const mediantmp = temps[0]
-console.log(`Median temp: ${mediantmp}`)
+const iss = secrets.serviceAccount
+console.log("iss " + iss)
+const isskey = secrets.serviceAccoutKey
+console.log("isskey " + isskey)
+const projectId = secrets.googleProjectId
+const queryStatement =
+  'SELECT * FROM `bigquery-public-data.noaa_gsod.gsod20*` WHERE CAST(YEAR AS INT64) = 2023 AND CAST(MO AS INT64) = 5   AND CAST(DA AS INT64) = 1  and stn="486980";'
 
-return Functions.encodeUint256(Math.round(mediantmp))
+const requestConfig = {
+  method: "post",
+  url: `https://bigquery.googleapis.com/bigquery/v2/projects/${projectId}/queries`,
+  headers: {
+    Authorization: `Bearer ${await getOauthToken(iss, isskey)}`,
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  data: {
+    query: queryStatement,
+    useLegacySql: false,
+  },
+}
+
+const request1 = Functions.makeHttpRequest(requestConfig)
+
+const responses = await Promise.all([request1])
+
+const temp = responses[0].data.rows[0].f[6].v
+
+return Functions.encodeUint256(Math.round(temp))
--- README.md
+++ README.md
@@ -12,7 +12,18 @@ The NFT smart contract is different from day 1. Redploy the NFT smart contract a
 - Set environment variable for
   - `npx env-enc set-pw` and enter a password
   - `npx env-enc set` add `POLYGONSCAN_API_KEY`, `PRIVATE_KEY` and `POLYGON_MUMBAI_RPC_URL`.
-- Get free Apikey from [ACCUWeather](https://developer.accuweather.com/), and then upadte the key in the file `API-request-example.js`.
+- 2 external data sources:
+  - Get free Apikey from [ACCUWeather](https://developer.accuweather.com/), and then upadte the key in the file `API-request-example.js`.
+  - The second way to fetch data is through Google big query. In order to use Google big query, you need to create a [google bigquery project](https://console.cloud.google.com/bigquery) on [GCP(google cloud protocol)](https://cloud.google.com/gcp?hl=en)
+  - Then create a [google service account](https://console.cloud.google.com/iam-admin/iam) and generate a pair of keys for the account.
+  - use `npx env-enc set` add `GOOGLE_PROJECT_ID`, `GOOGLE_ISS` and `GOOGLE_KEY`. 
+  - The example for `GOOGLE_PROJECT_ID` is `projectname-id`
+  - The example of `GOOGLE_ISS` is `serviceaccount@projectId.iam.gserviceaccount.com`
+  - The example of `ISS_KEY` is `-----BEGIN PRIVATE KEY-----\n privkey\n-----END PRIVATE KEY-----\n`. 
+  - Because secrets are used in js file executed by DON, we need add a new env variable GITHUB_API_TOKEN
+  - use `npx env-enc set` add `GITHUB_API_TOKEN`. 
+  - The example of `GITHUB_API_TOKEN` is `github_pat_xxx_xxx`
+
 - Run `npx hardhat functions-deploy-client --network polygonMumbai --verify true` to deploy smart contract `FunctionsConsumer.sol` and get address of smart contract `FunctionsConsumer`.
 - Go to polygonscan and find the smart contract just deployed, and call function `setNft` to set NFT address.
 - Run `npx hardhat functions-sub-create --network polygonMumbai --amount 5 --contract <FunctionsConsumer address>` get a subId.
